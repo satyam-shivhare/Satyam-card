30c844c885eb15955d8617183df4b5a7c80e22ff
Satyam Shivhare <satyamshivhare@Satyams-MacBook-Air.local>
2023-11-15 18:26:37 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source = {require('./assets/profile.jpeg')} style={styles.profile} />
+      <Text style={styles.text}>Hello</Text>
+      <Text style={styles.tagline}>I am Satyam</Text>
+      <Text style={styles.description} >iOS Developer</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  profile: {
+    width : 200,
+    height: 200,
+    borderRadius : 100,
+    margin: 30,
+  },
+  tagline : {
+    fontSize : 35,
+    textAlign : 'center',
+    fontWeight : 'bold',
+    margin : 10,
+  },
+  text : {
+    fontSize : 25,
+    fontWeight : 'light',
+    margin : 10,
+    textAlign : 'center',
+    fontFamily : 'Times New Roman'
+  },
+  description : {
+    fontSize  : 25,
+    textAlign: 'center',
+    fontWeight : '400',
+    fontFamily : 'Helvetica',
+    margin : 10
+  }
 });
